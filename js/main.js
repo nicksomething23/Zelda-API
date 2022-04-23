a68d2b318d5ed46891331e957adb85a6bd17ca1c
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-04-22 21:39:31 -0400

    upload api practice, Zelda Compendium

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = `https://botw-compendium.herokuapp.com/api/v2/entry/${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        console.log(data.data.name)
+        let objData = data.data;
+        
+        document.querySelector('h2').innerText = objData.name
+        document.querySelector('img').src = objData.image
+        document.querySelector('#explanation').innerText = objData.category
+        document.querySelector('#locations').innerText = objData.common_locations
+        document.querySelector('#drops').innerText = objData.drops
+        document.querySelector('#description').innerText = objData.description
+       
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
+fetch(`https://botw-compendium.herokuapp.com/api/v2/all`)
+.then(res => res.json()) // parse response as JSON
+.then(data => {
+  console.log(data)
+
+})
+.catch(err => {
+    console.log(`error ${err}`)
+});
